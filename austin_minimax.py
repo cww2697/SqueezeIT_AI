264cc65025178ada3e5ea6728a9167750ea05b5f
Austin Maddox <71454458+amaddox5@users.noreply.github.com>
2020-09-24 17:16:07 -0500

    Fixed bugs in minimax, added basic heuristic and debug output file

--- austin_minimax.py
+++ austin_minimax.py
@@ -12,17 +12,26 @@
 # where (x, y) is the location of the piece to be moved, and (a, b) 
 # is the new location
 
+from copy import deepcopy
+import datetime
+
 def get_next_move(board, player):
     # Set how deep we want to go
     depth = 3
 
+    debug_file = open('austin_minimax_debug.txt', 'w')
+    debug_file.write(datetime.datetime.fromtimestamp(datetime.datetime.now().timestamp()).isoformat())
+    debug_file.write('\n\n')
+    debug_file.close()
+
     return minimax(board, player, depth, 1)
 
 def minimax(board, player, depth, current_level):
-    debugModifer = ""
+    debugModifer = ''
+    debug_file = open('austin_minimax_debug.txt', 'a')
 
-    for i in range(0, current_level, 1):
-        debugModifer += "\t"
+    for i in range(0, current_level-1, 1):
+        debugModifer += '\t'
 
     if depth < current_level:
         # We are at the bottom of the tree, time to propigate back up
@@ -32,17 +41,18 @@ def minimax(board, player, depth, current_level):
         bestHeuristic = -99 if current_level % 2 != 0 else 99
 
         # For each player piece, for each possible move, recursively call minimax() with current_level + 1
-        for x in range(0, 7, 1):
-            for y in range(0, 7, 1):
+        for x in range(0, 8, 1):
+            for y in range(0, 8, 1):
                 if board[y][x] == player:
-                    # Generate every possible move this player can do (limit search to just row and column)
+                    # Generate every possible move this piece can do (limit search to just row and column)
                     
                     # Check vertical moves
-                    for newY in range(0, 7, 1):
+                    for newY in range(0, 8, 1):
                         # Make a new move
                         currentMove = (x, y, x, newY)
 
-                        print(debugModifer, "Considering", currentMove)
+                        print(debugModifer, 'Considering', currentMove)
+                        debug_file.write(debugModifer + ' Considering ' + str(currentMove) + '\n')
 
                         # If it is a valid move, 
                         if is_valid_move(board, player, currentMove):
@@ -51,24 +61,30 @@ def minimax(board, player, depth, current_level):
                             if current_level % 2 != 0:
                                 # Odd level, so maximize
                                 if currentHeuristic > bestHeuristic:
-                                    print(debugModifer, currentMove, "is better than", bestMove)
+                                    print(debugModifer, currentMove, 'is better than', bestMove)
+                                    debug_file.write(debugModifer + str(currentMove) + 'is better than' + str(bestMove) + '\n')
                                     bestMove = currentMove
                                     bestHeuristic = currentHeuristic
                             else:
                                 # Even level, so minimize
                                 if currentHeuristic < bestHeuristic:
-                                    print(debugModifer, currentMove, "is better than", bestMove)
+                                    print(debugModifer, currentMove, 'is better than', bestMove)
+                                    debug_file.write(debugModifer + str(currentMove) + 'is better than' + str(bestMove) + '\n')
                                     bestMove = currentMove
                                     bestHeuristic = currentHeuristic
                         else:
-                            print(debugModifer, currentMove, "is not a valid move")
+                            print(debugModifer, currentMove, 'is not a valid move')
+                            debug_file.write(debugModifer + str(currentMove) + 'is not a valid move\n')
                         
                         
 
                     # Check horizontal moves
-                    for newX in range(0, 7, 1):
+                    for newX in range(0, 8, 1):
                         # Make a new move
                         currentMove = (x, y, newX, y)
+
+                        print(debugModifer, 'Considering', currentMove)
+                        debug_file.write(debugModifer + ' Considering ' + str(currentMove) + '\n')
 
                         # If it is a valid move, 
                         if is_valid_move(board, player, currentMove):
@@ -77,25 +93,32 @@ def minimax(board, player, depth, current_level):
                             if current_level % 2 != 0:
                                 # Odd level, so maximize
                                 if currentHeuristic > bestHeuristic:
-                                    print(debugModifer, currentMove, "is better than", bestMove)
+                                    print(debugModifer, currentMove, 'is better than', bestMove)
+                                    debug_file.write(debugModifer + str(currentMove) + 'is better than' + str(bestMove) + '\n')
                                     bestMove = currentMove
                                     bestHeuristic = currentHeuristic
                             else:
                                 # Even level, so minimize
                                 if currentHeuristic < bestHeuristic:
-                                    print(debugModifer, currentMove, "is better than", bestMove)
+                                    print(debugModifer, currentMove, 'is better than', bestMove)
+                                    debug_file.write(debugModifer + str(currentMove) + 'is better than' + str(bestMove) + '\n')
                                     bestMove = currentMove
                                     bestHeuristic = currentHeuristic
                         else:
-                            print(debugModifer, currentMove, "is not a valid move")
+                            print(debugModifer, currentMove, 'is not a valid move')
+                            debug_file.write(debugModifer + str(currentMove) + 'is not a valid move\n')
                     
                     # If we are propigating, return the best heuristic
                     # If we are done propigating, return the best move we found
                     if current_level == 1:
-                        print(debugModifer, "Best move:", bestMove)
+                        print(debugModifer, 'Best move:', bestMove, 'with Heuristic:', bestHeuristic)
+                        debug_file.write(debugModifer + 'Best move: ' + str(bestMove) + ' with Heuristic ' + str(bestHeuristic) + '\n')
+                        debug_file.close()
                         return bestMove
                     else:
-                        print(debugModifer, "Best Heuristic at level", str(current_level) + ":", bestHeuristic)
+                        print(debugModifer, 'Best Heuristic at level', str(current_level) + ':', bestHeuristic)
+                        debug_file.write(debugModifer + 'Best Heuristic at level ' + str(current_level) + ': ' + str(bestHeuristic) + '\n')
+                        debug_file.close()
                         return bestHeuristic
 
 # Heuristic function that judges the value of a particular game state 
@@ -120,13 +143,57 @@ def heuristic(board, player):
     #  - If winning is the only objective, time wasting is a perfectly 
     #    viable strategy if we get a lead
     #      
-    return 0
 
+    # Temp Heuristic: Count how many pieces we have left and how many the opponent
+    # has left and value the board like that
+    counter = 0
+    opponent = 'W' if player == 'B' else 'B'
+
+    for x in range(0, 8, 1):
+        for y in range(0, 8, 1):
+            if board[y][x] == player:
+                counter += 1
+    
+    for x in range(0, 8, 1):
+        for y in range(0, 8, 1):
+            if board[y][x] == opponent:
+                counter -= 1
+
+    return counter
+
+# Given a move, make sure its valid, make the move, and resolve any squeezes
 def make_move(board, player, move):
-    newBoard = board
+    newBoard = deepcopy(board)
+    opponent = 'W' if player == 'B' else 'B'
 
-    newBoard[move[1]][move[0]] = ' '
-    newBoard[move[3]][move[2]] = player
+    if is_valid_move(board, player, move):
+        newBoard[move[1]][move[0]] = ' '
+        newBoard[move[3]][move[2]] = player
+
+        # Resolve any squeezes, prioritizing player GETTING SQUEEZED first
+        for x in range(0, 8, 1):
+            for y in range(0, 8, 1):
+                if board[y][x] == player:
+                    # Check up and down
+                    if y > 0 and y < 7 and board[y-1][x] == opponent and board[y+1][x] == opponent:
+                        print('Player lost piece')
+                        board[y][x] = ' '
+                    # Check left and right
+                    if x > 0 and x < 7 and board[y][x-1] == opponent and board[y][x+1] == opponent:
+                        print('Player lost piece')
+                        board[y][x] = ' '
+        
+        for x in range(0, 8, 1):
+            for y in range(0, 8, 1):
+                if board[y][x] == opponent:
+                    # Check up and down
+                    if y > 0 and y < 7 and board[y-1][x] == player and board[y+1][x] == player:
+                        print('Opponent lost piece')
+                        board[y][x] = ' '
+                    # Check left and right
+                    if x > 0 and x < 7 and board[y][x-1] == player and board[y][x+1] == player:
+                        print('Opponent lost piece')
+                        board[y][x] = ' '
 
     return newBoard
 
@@ -139,11 +206,11 @@ def is_valid_move(board, player, move):
 
     if board[pieceY][pieceX] != player:
         # Chosen piece does not belong to the player or is not there, so invalid
-        #print("Not Player Piece")
+        #print('Not Player Piece')
         return False
     elif pieceX == new_locX and pieceY == new_locY:
         # Not moving the piece, so invalid
-        #print("Not Moving")
+        #print('Not Moving')
         return False
     elif pieceX == new_locX:
         # Moving Up or down
@@ -151,20 +218,18 @@ def is_valid_move(board, player, move):
         # Look up or down to make sure no pieces between the piece and its new location
         if pieceY > new_locY:
             # Moving Up
-            #print("Moving Up")
+            #print('Moving Up')
             for loc in board[new_locY:pieceY]:
-                #print(loc[pieceX])
                 if loc[pieceX] != ' ':
-                    #print("Piece In Way")
+                    #print('Piece In Way')
                     return False
             return True
         else:
             # Moving down
-            #print("Moving Down")
+            #print('Moving Down')
             for loc in board[pieceY+1:new_locY+1]:
-                #print(loc[pieceX])
                 if loc[pieceX] != ' ':
-                    #print("Piece In Way")
+                    #print('Piece In Way')
                     return False
             return True
     elif pieceY == new_locY:
@@ -173,23 +238,21 @@ def is_valid_move(board, player, move):
         # Look left or right to make sure no pieces between the piece and its new location
         if pieceX > new_locX:
             # Moving left
-            #print("Moving Left")
+            #print('Moving Left')
             for loc in board[pieceY][new_locX:pieceX]:
-                #print(loc)
                 if loc != ' ':
-                    #print("Piece In Way")
+                    #print('Piece In Way')
                     return False
             return True
         else:
             # Moving right
-            #print("Moving Right")
+            #print('Moving Right')
             for loc in board[pieceY][pieceX+1:new_locX+1]:
-                #print(loc)
                 if loc != ' ':
-                    #print("Piece In Way")
+                    #print('Piece In Way')
                     return False
             return True
     else:
         # Piece is not moving in a straight line, so invalid
-        #print("Not moving in a straight line")
+        #print('Not moving in a straight line')
         return False
