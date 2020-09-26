87af852f5d3f819abb99220fd08cc00c5d21aae8
Cody West <cww2697@gmail.com>
2020-09-26 18:57:48 -0500

    Add comments to source

--- IO.py
+++ IO.py
@@ -1,14 +1,18 @@
 import numpy as np
 
 def write_file(gamestate):
-    fhandle_write = open('gamestate.txt', 'w')
+    # Open gamestate file
+    fhandle_write = open('gamestate.txt', 'w') 
+    # Write current move gamestate to gamestate file
     for x in gamestate:
         writerow(x)
-    fhandle_write.close()
+    fhandle_write.close() # close file
 
 def read_file(gamestate):
+    # Open gamestate file for reading
     with open('gamestate.txt') as fhandle_read:
+        # Split lines and convert back to array for analysis
         gamestate = [line.split() for line in fhandle_read]
-    fhandle_read.close()
+    fhandle_read.close() # close file
 
 
