ae666b6bade8fcc76ef08e8ac25aa004ed1e3d75
Cody West <cww2697@gmail.com>
2020-09-29 13:32:16 -0500

    Add base defensive_heuristics

--- heurisitcs.py
+++ heurisitcs.py
@@ -15,3 +15,25 @@ def simple_heuristic(board, player):
                 counter -= 1
 
     return counter
+
+# Defensive heuristic
+# Encourages diagonals
+# Discourages single spaces in between pieces and multiple pieces next to each other
+
+
+def defensive_heuristic(board, player):
+    counter = 0
+    opponent = 'W' if player == 'B' else 'B'
+
+    for x in range(0,8, 1):
+        for y in range(0, 8 , 1):
+            if board[y][x] == board[y][x+2]:
+                counter -= 1
+    for x in range(0,8, 1):
+        for y in range(0, 8 , 1):
+            if board[y][x] == board[y][x+2]:
+                counter -= 1
+    for x in range(0,8, 1):
+        for y in range(0, 8 , 1):
+            if board[y][x] == board[y+1][x+1]:
+                counter += 1
